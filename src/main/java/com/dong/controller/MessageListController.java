064ede5b43dd2ec99170fa8133e88d1d738ed69e
Chuan Dong <dongchuan55@gmail.com>
2014-08-07 22:14:19 +0200

    [Add] MessageListController

---
+++ src/main/java/com/dong/controller/MessageListController.java
@@ -0,0 +1,49 @@
+package com.dong.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.dong.bean.Message;
+import com.dong.dao.MessageDAO;
+import com.dong.factory.MessageDAOFactory;
+import com.dong.util.Page;
+import com.dong.util.PageUtil;
+
+@Controller
+public class MessageListController {
+	
+	@RequestMapping(value = "/MessageList", method = RequestMethod.GET) 
+	public String showMsgListPage() {
+		return "msgList";
+	}
+	
+	@RequestMapping(value = "/GetMessageList", method = RequestMethod.GET)
+	public ModelAndView showMsgList(@RequestParam("currentPage") String currentPageStr) {
+		
+		int currentPage = 0;
+		
+		if(currentPageStr == null || "".equals(currentPageStr)){
+			currentPage = 1;
+		}else {
+			currentPage = Integer.parseInt(currentPageStr);
+		}
+		
+		// Get message from DB
+		MessageDAO messageDAO = MessageDAOFactory.getMessageAOInstance();
+		Page page = PageUtil.createPage(5, messageDAO.findAllCount(), currentPage);
+		List<Message> messages = messageDAO.findAllMessagee(page);
+		
+		Map<String,Object> rpAttribute= new HashMap<String,Object>();
+		rpAttribute.put("messageList", messages);
+		rpAttribute.put("page", page);
+
+		return new ModelAndView("msgList","messageList",rpAttribute);
+	}
+}

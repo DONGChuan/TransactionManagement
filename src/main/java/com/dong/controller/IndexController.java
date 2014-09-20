6b65dd9010963e9cc324822b71a9bc112a2198b5
Chuan Dong <dongchuan55@gmail.com>
2014-09-20 17:22:20 +0200

    [Update] IndexController

--- src/main/java/com/dong/controller/IndexController.java
+++ src/main/java/com/dong/controller/IndexController.java
@@ -2,34 +2,30 @@ package com.dong.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.dong.dao.MessageDAO;
-import com.dong.factory.MessageDAOFactory;
+import com.dong.bo.MessageBo;
+import com.dong.model.Message;
 import com.dong.util.Page;
 import com.dong.util.PageUtil;
-import com.dong.bean.Message;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
-	/*@RequestMapping(method = RequestMethod.GET) 
-	public String showLoginPage(){
-		return "index";
-	}*/
+	@Autowired
+    private MessageBo messageBo;
 	
 	@RequestMapping(value={"index","/"} , method = RequestMethod.GET)
 	public ModelAndView showIndex()	{
 		ModelAndView modelView = new ModelAndView();
 		
-		// Get messages from DB
-		MessageDAO messageDAO = MessageDAOFactory.getMessageAOInstance();
-		Page pageX = PageUtil.createPage(6, messageDAO.findAllCount(), 1);
-		List<Message> messages = messageDAO.findAllMessagee(pageX);
+		Page pageX = PageUtil.createPage(6, messageBo.findAllCount(), 1);
+		List<Message> messages = messageBo.findAllMessagee(pageX);
 		
 		// Return view with messages object 
 		modelView.addObject("messages", messages);		
